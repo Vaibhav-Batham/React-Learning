c3ae61759d0f70b9103f0eb5fe828198a751b6fa
Vaibhav Batham <vaibhavbatham4@gmail.com>
2026-03-05 12:23:16 +0530

    react learning

---
+++ project1/src/App.jsx
@@ -0,0 +1,19 @@
+ 
+ import Nav from "./components/Nav"
+ import Main from "./components/Hero"
+ import Footer from "./components/Footer"
+
+ 
+ 
+ function App(){
+
+  return (
+  <>
+  <Nav/>
+  <Hero/>
+  <Footer/>
+  </>
+  )
+ }
+
+ export default App
